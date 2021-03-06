eddeb04e66f43c80788d2ddae92c56f2168c09bf
wjl@imac <cougar2008@gmail.com>
2021-03-07 01:08:57 +0800

    fix add python client

--- xds-server/internal/app/callback.go
+++ xds-server/internal/app/callback.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"sync"
 
-	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	//v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	v2 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 
 	logger "github.com/asishrs/proxyless-grpc-lb/common/pkg/logger"
 	"go.uber.org/zap"
